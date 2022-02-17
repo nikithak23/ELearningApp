6bd0ad0bf49709d2d7f34ff35654fe71cf08e6a3
Nikitha K <nikikudupu23@gmail.com>
2022-02-17 17:06:31 +0530

    modification

--- src/Screens/SignUpScreen.js
+++ src/Screens/SignUpScreen.js
@@ -14,10 +14,9 @@ const SignUp = () => {
       <Text style={styles.title}>SignUp Page</Text>
       <Text style={styles.title}>hii</Text>
       <Text style={styles.title}>Hello, Welcome</Text>
-
       <Text style={styles.title}>Hello, Welcome 123</Text>
       <Text style={styles.title}>E Learnng App</Text>
-      
+
       
     </View>
   );
