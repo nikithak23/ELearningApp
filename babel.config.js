df4d9a953cb7410bba4ac10b0e00ae4ac2fd9072
Afiya Nafeesa <afiyanafeesaa@gmail.com>
2022-02-17 19:09:35 +0530

    committed

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-
-
-
-
-  
   //CAUTION: Reanimated plugin has to be listed last.
   plugins: [
     'react-native-reanimated/plugin',
