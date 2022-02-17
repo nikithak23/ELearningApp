9da130c97e1fa26ef700f2e5f4daf4f9d3435176
Rakshith N <raksithnraksh@gmail.com>
2022-02-17 16:17:00 +0530

    pushed

---
+++ src/Screens/SignUpScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, View, StyleSheet, Image, ScrollView} from 'react-native';
+
+const SignUp = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>SignUp Page 1</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor:'#4C93FF',
+  },
+  title: {
+    fontSize: 16,
+    color: 'black',
+  },
+});
+export default SignUp;
