adae5f886ebeb140e9b26d5577cf6c098eaa0c91
Rakshith N <raksithnraksh@gmail.com>
2022-03-02 11:24:29 +0530

    created courses page

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import {name as appName} from './app.json';
 
 // import App from './App';
 import mainNavig from './src/mainNavig';
+import ProfileScreen from './src/Screens/ProfileScreen';
 import ResultsScreen from './src/Screens/ResultScreen';
 import Dropdown from './src/components/Dropdown';
 // import AuthenticationScreen from './src/Screens/SignInScreen';
